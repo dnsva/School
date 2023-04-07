332803be55b2fef0d6730c264799d87c51725b0c
Anna <peachtiger0@gmail.com>
2023-04-07 19:50:01 -0400

    ok

--- Phone Book/main.cpp
+++ Phone Book/main.cpp
@@ -45,18 +45,20 @@ void name_sort();       //
 
 int main(){
 
-    cout<<"┬ ┬┌─┐┬ ┬┬─┐  ┬  ┬┌─┐┬─┐┬ ┬  ┌─┐┬ ┬┌┐┌ \n";
-    cout<<"└┬┘│ ││ │├┬┘  └┐┌┘├┤ ├┬┘└┬┘  │ │││││││ \n";
-    cout<<" ┴ └─┘└─┘┴└─   └┘ └─┘┴└─ ┴   └─┘└┴┘┘└┘ \n";
-    cout<<"    ┌─┐┬ ┬┌─┐┌┐┌┌─┐  ┌┐ ┌─┐┌─┐┬┌─      \n";
-    cout<<"    ├─┘├─┤│ ││││├┤   ├┴┐│ ││ │├┴┐      \n";
-    cout<<"    ┴  ┴ ┴└─┘┘└┘└─┘  └─┘└─┘└─┘┴ ┴      \n";
-
-    
+    /* THE FOLLOWING LINES PRINT ASCII ART */
+    cout<<" __   __             __   __               ___                   \n";
+    cout<<" \\ \\ / /__ _  _ _ _  \\ \\ / /__ _ _ _  _   / _ \\__ __ ___ _       \n";
+    cout<<"  \\ V / _ \\ || | '_|  \\ V / -_) '_| || | | (_) \\ V  V / ' \\ \n";
+    cout<<"   |_|\\___/\\_,_|_|     \\_/\\___|_|  \\_, |  \\___/ \\_/\\_/|_||_|     \n";
+    cout<<"           ___ _                 _ |__/         _                \n";
+    cout<<"          | _ \\ |_  ___ _ _  ___| |__  ___  ___| |__             \n";
+    cout<<"          |  _/ ' \\/ _ \\ ' \\/ -_) '  \\/ _ \\/ _ \\ / /          \n";
+    cout<<"          |_| |_||_\\___/_||_\\___|_.__/\\___/\\___/_\\_\\             \n";
+                                                            
     while(true){
         int menu_option;
 
-        cout<<"----------------------------------\n";
+        cout<<"----------------------------------------------------------------\n";
         cout<<"[0] - Add Contact\n";
         cout<<"[1] - Remove Contact\n";
         cout<<"[2] - Edit Contact\n";
@@ -68,11 +70,11 @@ int main(){
         cin>>menu_option;
 
         while(menu_option < 0 || menu_option > 5){
-            cout<<"Bad input try again:\n> ";
+            cout<<"  -> Bad input try again: ";
             cin>>menu_option;
         }
 
-        cout<<"----------------------------------\n";
+        cout<<"----------------------------------------------------------------\n";
 
         if(menu_option == 0){
             add_contact();
@@ -109,10 +111,10 @@ void add_contact(){ //adds a person
     string temp_name; //name
     string temp_num;  //phone
     string temp_add;  //address
-    bool good_case = false;   //for address error check
     bool found_space = false; //for address error check
     string temp_city; //city
     string temp_pc;   //postal code 
+    bool good_input = true; //for error checks
 
     for(int i = 0; i<5; ++i){  //for each person
         if(people[i].exists)   //if they exist
@@ -129,6 +131,20 @@ void add_contact(){ //adds a person
     cout<<"-> Enter name: "; //ask
     cin.ignore(); //ignore ws
     getline(cin, temp_name); //get
+    while(1){
+        good_input = true;
+        for(int i = 0; i<temp_name.length(); ++i){
+            if(!isalpha(temp_name.at(i)) && temp_name.at(i) != ' '){
+                good_input = false;
+                cout<<"  -> Bad input. Try again: "; 
+                getline(cin, temp_name);
+                break;
+            }
+        }
+        if(good_input){
+            break;
+        }
+    }
     strncpy(new_person.name, temp_name.c_str(), 20); //truncate if too long
     new_person.name[19] = '\0'; //null
 
@@ -136,14 +152,19 @@ void add_contact(){ //adds a person
 
     cout<<"-> Enter phone number (NO SPACES, NO FORMATTING, NOTHING - JUST THE NUMBERS): "; //ask
     getline(cin, temp_num); //get
-    while(temp_num.length() != 10){ //check for correct size
-        cout<<"  -> Bad input. Try again: "; //err msg
-        getline(cin, temp_num); //get again
-        
-    }
-    while(!(isdigit(temp_num.at(0)) && isdigit(temp_num.at(1)) && isdigit(temp_num.at(2)) && isdigit(temp_num.at(3)) && isdigit(temp_num.at(4)) && isdigit(temp_num.at(5)) && isdigit(temp_num.at(6)) && isdigit(temp_num.at(7)) && isdigit(temp_num.at(8)) && isdigit(temp_num.at(9)))){ //check
-        cout<<"  -> Only numbers allowed. Try again: "; //err msg
-        getline(cin, temp_num); //get again
+    while(1){
+        good_input = true;
+        if(temp_num.length() != 10){
+            good_input = false;
+        }else if(!(isdigit(temp_num.at(0)) && isdigit(temp_num.at(1)) && isdigit(temp_num.at(2)) && isdigit(temp_num.at(3)) && isdigit(temp_num.at(4)) && isdigit(temp_num.at(5)) && isdigit(temp_num.at(6)) && isdigit(temp_num.at(7)) && isdigit(temp_num.at(8)) && isdigit(temp_num.at(9)))){
+            good_input = false;
+        }
+        if(!good_input){
+            cout<<"  -> Bad input. Try again: ";
+            getline(cin, temp_num);
+        }else{
+            break;
+        }
     }
     strcpy(new_person.number, temp_num.c_str()); //make cstr
     new_person.number[10] = '\0'; //null
@@ -152,7 +173,8 @@ void add_contact(){ //adds a person
 
     cout<<"-> Enter address: "; //ask
     getline(cin, temp_add); //get
-    while(!good_case){ //while bad input
+    while(1){ //while bad input
+        good_input = true; //assume for now 
         found_space = false; //between numbers and streetname
         for(int i = 0; i<temp_add.length(); ++i){ //for each char
             if(temp_add.at(i) == ' '){ //if space then letters should follow
@@ -160,20 +182,22 @@ void add_contact(){ //adds a person
             }
             if(!found_space){ //has to be digit
                 if(!isdigit(temp_add.at(i))){ //if bad
-                    good_case = false; //not good
+                    good_input = false; //not good
                     break; //get out of char check loop
                 }
             }else{ //has to be alpha or a space
-                if(!(isalpha(temp_add.at(i)) || temp_add.at(i) == ' ')){ //check
-                    good_case = false; //not good
+                if(!isalpha(temp_add.at(i)) && temp_add.at(i) != ' '){ //check
+                    good_input = false; //not good
                     break; //get out of char check loop
                 }
             }
-            good_case = true; //if passed those checks then true
+            good_input = true; //ok
         }
-        if(!good_case){ //if bad
-            cout<<"  -> Bad format. Try again: "; //err msg
-            getline(cin, temp_add); //get 
+        if(!good_input ||  !found_space){ //has to be good input AND not only just numbers
+            cout<<"  -> Bad input. Try again: ";
+            getline(cin, temp_add);
+        }else{
+            break;
         }
     }
     strncpy(new_person.address, temp_add.c_str(), 30); //trunc if too long
@@ -183,6 +207,20 @@ void add_contact(){ //adds a person
 
     cout<<"-> Enter city: "; //ask
     getline(cin, temp_city); //get
+    while(1){
+        good_input = true; //for now 
+        for(int i = 0; i<temp_city.length(); ++i){
+            if(!isalpha(temp_city.at(i)) && temp_city.at(i) != ' '){
+                good_input = false;
+                cout<<"  -> Bad input. Try again: ";
+                getline(cin, temp_city);
+                break;
+            }
+        }
+        if(good_input){
+            break;
+        }
+    }
     strncpy(new_person.city, temp_city.c_str(), 20); //truncate if too long
     new_person.city[19] = '\0'; //null
 
@@ -190,13 +228,19 @@ void add_contact(){ //adds a person
 
     cout<<"-> Enter postal code (NO SPACES, NO FORMATTING, NOTHING): "; //ask
     getline(cin, temp_pc); //get
-    while(temp_pc.length() != 6){ //check size
-        cout<<"  -> Postal code is in wrong format. Try again: "; //err msg
-        getline(cin, temp_pc); //get again
-    }
-    while(!(isalpha(temp_pc.at(0)) && isdigit(temp_pc.at(1)) && isalpha(temp_pc.at(2)) && isdigit(temp_pc.at(3)) && isalpha(temp_pc.at(4)) && isdigit(temp_pc.at(5)))){ //check 
-        cout<<"  -> Has to be letter, number, letter, etc. Try again: "; //err message
-        getline(cin, temp_pc); //get again
+    while(1){
+        good_input = true;
+        if(temp_pc.length() != 6){ //check size
+            good_input = false;
+        }else if(!(isalpha(temp_pc.at(0)) && isdigit(temp_pc.at(1)) && isalpha(temp_pc.at(2)) && isdigit(temp_pc.at(3)) && isalpha(temp_pc.at(4)) && isdigit(temp_pc.at(5)))){ //check 
+            good_input = false;
+        }
+        if(good_input){
+            break;
+        }else{
+            cout<<"  -> Bad input. Try again: ";
+            getline(cin, temp_pc);
+        }
     }
     strcpy(new_person.pc, temp_pc.c_str()); //add
     new_person.pc[6] = '\0'; //null
@@ -256,10 +300,10 @@ void edit_contact(){
     string temp_name; //name
     string temp_num;  //phone
     string temp_add;  //address
-    bool good_case = false;   //for address error check
     bool found_space = false; //for address error check
     string temp_city; //city
     string temp_pc;   //postal code 
+    bool good_input = true; //for error checks
 
     for(int i = 0; i<5; ++i){ //display all contacts
         if(people[i].exists){ //if they exist
@@ -287,7 +331,7 @@ void edit_contact(){
         cout<<"(Enter -1 to exit edit mode)\n> "; //extra
         cin>>edit_choice; //get
         while(edit_choice < -1 || edit_choice > 4){ //err check
-            cout<<"  -> Bad input. Try again\n> "; //err msg
+            cout<<"  -> Bad input. Try again: "; //err msg
             cin>>edit_choice; //get again
         }
         if(edit_choice == -1){ //QUIT
@@ -298,6 +342,20 @@ void edit_contact(){
             cout<<"-> Enter name: "; //ask
             cin.ignore(); //ignore ws
             getline(cin, temp_name);
+            while(1){
+                good_input = true;
+                for(int i = 0; i<temp_name.length(); ++i){
+                    if(!isalpha(temp_name.at(i)) && temp_name.at(i) != ' '){
+                        good_input = false;
+                        cout<<"  -> Bad input. Try again: "; 
+                        getline(cin, temp_name);
+                        break;
+                    }
+                }
+                if(good_input){
+                    break;
+                }
+            }
             strncpy(people[edit_index].name, temp_name.c_str(), 20); //truncate if too long
             people[edit_index].name[19] = '\0'; //null
 
@@ -305,14 +363,19 @@ void edit_contact(){
             cout<<"-> Enter phone number (NO SPACES, NO FORMATTING, NOTHING - JUST THE NUMBERS): "; //ask
             cin.ignore(); //ignore ws
             getline(cin, temp_num); //get
-            while(temp_num.length() != 10){ //check for correct size
-                cout<<"  -> Bad input. Try again: "; //err msg
-                getline(cin, temp_num); //get again
-                
-            }
-            while(!(isdigit(temp_num.at(0)) && isdigit(temp_num.at(1)) && isdigit(temp_num.at(2)) && isdigit(temp_num.at(3)) && isdigit(temp_num.at(4)) && isdigit(temp_num.at(5)) && isdigit(temp_num.at(6)) && isdigit(temp_num.at(7)) && isdigit(temp_num.at(8)) && isdigit(temp_num.at(9)))){ //check
-                cout<<"  -> Only numbers allowed. Try again: "; //err ms
-                getline(cin, temp_num); //get again
+            while(1){
+                good_input = true;
+                if(temp_num.length() != 10){
+                    good_input = false;
+                }else if(!(isdigit(temp_num.at(0)) && isdigit(temp_num.at(1)) && isdigit(temp_num.at(2)) && isdigit(temp_num.at(3)) && isdigit(temp_num.at(4)) && isdigit(temp_num.at(5)) && isdigit(temp_num.at(6)) && isdigit(temp_num.at(7)) && isdigit(temp_num.at(8)) && isdigit(temp_num.at(9)))){
+                    good_input = false;
+                }
+                if(!good_input){
+                    cout<<"  -> Bad input. Try again: ";
+                    getline(cin, temp_num);
+                }else{
+                    break;
+                }
             }
             strcpy(people[edit_index].number, temp_num.c_str()); //make cstr
             people[edit_index].number[10] = '\0';
@@ -321,28 +384,31 @@ void edit_contact(){
             cout<<"-> Enter address: "; //ask
             cin.ignore(); //ignore cin
             getline(cin, temp_add); //get
-            while(!good_case){ //while bad input
+            while(1){ //while bad input
+                good_input = true; //assume for now 
                 found_space = false; //between numbers and streetname
                 for(int i = 0; i<temp_add.length(); ++i){ //for each char
                     if(temp_add.at(i) == ' '){ //if space then letters should follow
                         found_space = true; //ok
                     }
                     if(!found_space){ //has to be digit
-                        if(!isdigit(temp_add.at(i))){ //if not digit
-                            good_case = false; //bad
-                            break; //get out of loop
+                        if(!isdigit(temp_add.at(i))){ //if bad
+                            good_input = false; //not good
+                            break; //get out of char check loop
                         }
                     }else{ //has to be alpha or a space
-                        if(!(isalpha(temp_add.at(i)) || temp_add.at(i) == ' ')){ //if digit
-                            good_case = false; //bad
-                            break; //get out of loop
+                        if(!isalpha(temp_add.at(i)) && temp_add.at(i) != ' '){ //check
+                            good_input = false; //not good
+                            break; //get out of char check loop
                         }
                     }
-                    good_case = true; //if all checks passed
+                    good_input = true; //ok
                 }
-                if(!good_case){ //if bad
-                    cout<<"  -> Bad format. Try again: "; //err msg
-                    getline(cin, temp_add); //get again
+                if(!good_input || !found_space){
+                    cout<<"  -> Bad input. Try again: ";
+                    getline(cin, temp_add);
+                }else{
+                    break;
                 }
             }
             strncpy(people[edit_index].address, temp_add.c_str(), 30); //trunc if too long
@@ -352,6 +418,20 @@ void edit_contact(){
             cout<<"-> Enter city: "; //ask
             cin.ignore(); //no ws
             getline(cin, temp_city); //get
+            while(1){
+                good_input = true; //for now 
+                for(int i = 0; i<temp_city.length(); ++i){
+                    if(!isalpha(temp_city.at(i)) && temp_city.at(i) != ' '){
+                        good_input = false;
+                        cout<<"  -> Bad input. Try again: ";
+                        getline(cin, temp_city);
+                        break;
+                    }
+                }
+                if(good_input){
+                    break;
+                }
+            }
             strncpy(people[edit_index].city, temp_city.c_str(), 20); //truncate if too long
             people[edit_index].city[19] = '\0'; //null
 
@@ -359,13 +439,19 @@ void edit_contact(){
             cout<<"-> Enter postal code (NO SPACES, NO FORMATTING, NOTHING): "; //ask
             cin.ignore(); //ignore ws
             getline(cin, temp_pc); //get
-            while(temp_pc.length() != 6){ //check size
-                cout<<"  -> Postal code is in wrong format. Try again: "; //err msg
-                getline(cin, temp_pc); //get again
-            }
-            while(!(isalpha(temp_pc.at(0)) && isdigit(temp_pc.at(1)) && isalpha(temp_pc.at(2)) && isdigit(temp_pc.at(3)) && isalpha(temp_pc.at(4)) && isdigit(temp_pc.at(5)))){ //check
-                cout<<"  -> Has to be letter, number, letter, etc. Try again: "; //err msg
-                getline(cin, temp_pc); //get again
+            while(1){
+                good_input = true;
+                if(temp_pc.length() != 6){ //check size
+                    good_input = false;
+                }else if(!(isalpha(temp_pc.at(0)) && isdigit(temp_pc.at(1)) && isalpha(temp_pc.at(2)) && isdigit(temp_pc.at(3)) && isalpha(temp_pc.at(4)) && isdigit(temp_pc.at(5)))){ //check 
+                    good_input = false;
+                }
+                if(good_input){
+                    break;
+                }else{
+                    cout<<"  -> Bad input. Try again: ";
+                    getline(cin, temp_pc);
+                }
             }
             strcpy(people[edit_index].pc, temp_pc.c_str()); //add
             people[edit_index].pc[6] = '\0'; //null
@@ -415,7 +501,7 @@ void display_all(){ //display everyone
         if(people[i].exists){ //if people exist
             people_exist = true; //set to yes
             people[i].display(); //display info
-            cout<<"++++++++++++++++++++++++++++++++++\n"; //border
+            cout<<"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"; //border
         }
     }
     if(!people_exist){ //you cant display anything if people don't exist
